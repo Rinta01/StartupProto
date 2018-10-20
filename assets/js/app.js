df15e37a1d173535e0cbcf44e248605a0ea02b09
Rinat Ganbarov <rinat.ganbarov@gmail.com>
2018-10-20 20:25:17 +0300

    scrolls

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,28 @@
-$("#contactsA").on("click",function(event) {
+$(".contactsA").on("click",function(event) {
     event.preventDefault();
 
     $('html, body').animate({
         scrollTop: $("#contacts").offset().top
+    }, 2000);
+
+    console.log("HI")
+});
+
+$("#toursA").on("click",function(event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $("#tours").offset().top
+    }, 2000);
+
+    console.log("HI")
+});
+
+$("#aboutA").on("click",function(event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
     }, 2000);
 
     console.log("HI")
